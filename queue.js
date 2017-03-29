c8f5c3d63149bfec24fc1d58020235cd24022db9
Bhirawa Mbani <bhirmbani@gmail.com>
2017-03-29 15:36:44 +0700

    first solution done stack and queue

--- queue.js
+++ queue.js
@@ -2,6 +2,30 @@
 
 class Queue {
   constructor () {
+    this.container = [];
+  }
 
+  insert(value) {
+    this.container.push(value);
+  }
+  // remove first element
+  remove() {
+    let removed = this.container.shift();
+    console.log(`${removed} removed!`);
+  }
+
+  display() {
+    console.log(this.container);
   }
 }
+
+let myQueue = new Queue();
+
+myQueue.insert('Makan');
+myQueue.insert('Belanja');
+myQueue.insert('Bernyanyi');
+myQueue.insert('Belajar');
+myQueue.insert('Bermain');
+myQueue.display();
+myQueue.remove();
+myQueue.display();
