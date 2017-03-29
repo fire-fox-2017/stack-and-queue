802c6467d0f2f20cddecc22bfe7b2d8693c3945d
fearzen3 <ivanhabi2@gmail.com>
2017-03-29 17:49:13 +0700

    semoga bener

--- queue.js
+++ queue.js
@@ -1,7 +1,88 @@
 'use strict'
 
+
+
 class Queue {
-  constructor () {
+  constructor() {
+    this._arr=[];
+    this.MAX_SIZE=5;
+  }
+
+  size() {
+    return 'panjang Queue ini : '+this._arr.length;
 
   }
+
+  queuePush(x) {
+
+    if(this.isFull()){
+      console.log(x);
+      this._arr.push(x);
+    }else{
+      console.log('antrian sudah full');
+
+    }
+
+  }
+
+  queuePop() {
+    if(this.isEmpty()){
+      console.log('antrian sudah habis!');
+    }else{
+      this._arr.shift();
+    }
+
+
+  }
+
+  first() {
+    return 'antrian pertama yang menunggu : '+this._arr[0];
+  }
+
+  last() {
+    return 'antrian terakhir yang menunggu : '+this._arr[this._arr.length-1];
+  }
+
+  isEmpty() {
+    if(this._arr.length>0){
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  isFull() {
+    if(this._arr.length<this.MAX_SIZE){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  queuePeep() {
+    return 'nilai paling atas : '+this._arr[this._arr.length-1];
+  }
+
 }
+var antrian = new Queue();
+antrian.queuePush(1);
+antrian.queuePush(2);
+antrian.queuePush(3);
+console.log(antrian.size());
+
+antrian.queuePush(4);
+antrian.queuePush(5);
+antrian.queuePush(6);
+antrian.queuePush(7);
+antrian.queuePush(8);
+console.log(antrian.first());
+console.log(antrian.last());
+antrian.queuePush(9);
+antrian.queuePop();
+antrian.queuePop();
+antrian.queuePop();
+antrian.queuePop();
+antrian.queuePop();
+antrian.queuePop();
+antrian.queuePop();
+antrian.queuePop();
--- stack.js
+++ stack.js
@@ -3,35 +3,88 @@
 // Your code implementation
 class Stack {
   constructor() {
+    this._arr=[];
+    this.MAX_SIZE=5;
   }
 
   size() {
+    return 'Besar Stack ini : '+this._arr.length;
+
   }
 
   stackPush(x) {
+
+    if(this.isFull()){
+      console.log(x);
+      this._arr.push(x);
+    }else{
+      console.log('Maaf sudah full');
+
+    }
+
   }
 
   stackPop() {
+    if(this.isEmpty()){
+      console.log('Maaf sudah habis!');
+    }else{
+      this._arr.pop();
+    }
+
+
   }
 
   first() {
+    return 'nilai pertama yang dimasukkan : '+this._arr[0];
   }
 
   last() {
+    return 'nilai terakhir yang dimasukkan : '+this._arr[this._arr.length-1];
   }
 
   isEmpty() {
+    if(this._arr.length>0){
+      return false;
+    }else{
+      return true;
+    }
   }
 
   isFull() {
+    if(this._arr.length<this.MAX_SIZE){
+      return true;
+    }else{
+      return false;
+    }
   }
 
   stackPeep() {
+    return 'nilai paling atas : '+this._arr[this._arr.length-1];
   }
 
-  // implement queue
-  queue() {
-  }
 }
+var stak = new Stack();
+stak.stackPush(1);
+stak.stackPush(2);
+stak.stackPush(3);
+console.log(stak.size());
+stak.stackPush(4);
+stak.stackPush(5);
+stak.stackPush(6);
+stak.stackPush(7);
+stak.stackPush(8);
+console.log(stak.last());
+console.log(stak.first());
+console.log(stak.stackPeep());
+stak.stackPush(9);
+stak.stackPop();
+stak.stackPop();
+stak.stackPop();
+stak.stackPop();
+stak.stackPop();
+stak.stackPop();
+stak.stackPop();
+stak.stackPop();
+
 
 module.exports = Stack
