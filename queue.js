a6d7e8d0118785575cf897c7502cf53fa49c4607
haryanawisnu <arya.wisnu05@gmail.com>
2017-03-29 17:22:10 +0700

    done

--- queue.js
+++ queue.js
@@ -1,7 +1,68 @@
 'use strict'
 
 class Queue {
-  constructor () {
-
+  constructor() {
+    this.queue=[];
+    this.maxSize=5;
+  }
+  queuePush(x) {
+    if(this.isFull()){
+      console.log("Gagal Menambahkan Karena Queue Penuh!")
+    }else {
+      this.queue.push(x);
+    }
+  }
+  queuePop() {
+    if(this.isEmpty()){
+      console.log("Gagal Menghapus karena Queue Kosong!");
+    }else {
+      this.queue.shift();
+    }
+  }
+  queuePeek() {
+    console.log(this.queue[0]);
+  }
+  first() {
+    console.log(this.queue[0]) ;
+  }
+  last() {
+    console.log(this.queue[this.queue.length-1]);
+  }
+  isEmpty() {
+    if (this.queue.length==0) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+  isFull() {
+    if (this.maxSize==this.queue.length) {
+      return true;
+    }else {
+      return false;
+    }
   }
 }
+
+let myQueue=new Queue();
+
+console.log(myQueue.isEmpty());
+
+myQueue.queuePush("JavaScript");
+myQueue.queuePush("is just so");
+myQueue.queuePush("cool");
+myQueue.queuePush(15);
+
+myQueue.queuePeek();
+
+myQueue.queuePop();
+myQueue.queuePop();
+myQueue.queuePop();
+
+console.log(myQueue.isEmpty());
+
+
+myQueue.queuePop();
+
+
+module.exports = Queue
--- stack.js
+++ stack.js
@@ -3,35 +3,66 @@
 // Your code implementation
 class Stack {
   constructor() {
+    this.stack=[];
+    this.maxSize=5;
   }
-
-  size() {
-  }
-
   stackPush(x) {
+    if(this.isFull()){
+      console.log("Gagal Menambahkan Karena Stack Penuh!")
+    }else {
+      this.stack.push(x);
+    }
   }
-
   stackPop() {
+    if(this.isEmpty()){
+      console.log("Gagal Menghapus karena Stack Kosong!");
+    }else {
+      this.stack=this.stack.slice(0,this.stack.length-1);
+    }
   }
-
+  stackPeek() {
+    console.log(this.stack[this.stack.length-1]);
+  }
   first() {
+    console.log(this.stack[this.stack.length-1]) ;
   }
-
   last() {
+    console.log(this.stack[0]);
   }
-
   isEmpty() {
+    if (this.stack.length==0) {
+      return true;
+    }else {
+      return false;
+    }
   }
-
   isFull() {
-  }
-
-  stackPeep() {
-  }
-
-  // implement queue
-  queue() {
+    if (this.maxSize==this.stack.length) {
+      return true;
+    }else {
+      return false;
+    }
   }
 }
+let myStack=new Stack();
+
+console.log(myStack.isEmpty());
+
+myStack.stackPush("JavaScript");
+myStack.stackPush("is just so");
+myStack.stackPush("cool");
+myStack.stackPush(15);
+
+myStack.stackPeek();
+
+myStack.stackPop();
+myStack.stackPop();
+myStack.stackPop();
+myStack.stackPop();
+
+console.log(myStack.isEmpty());
+
+myStack.stackPop();
+
 
 module.exports = Stack
