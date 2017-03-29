d3fe0413caa51cd0dab95aa86b71c985b74645fa
akbarsahata <akbarsahata@outlook.co.id>
2017-03-29 09:57:18 +0700

    Template

---
+++ stack.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// Your code implementation
+class Stack {
+  constructor() {
+  }
+
+  size() {
+  }
+
+  stackPush(x) {
+  }
+
+  stackPop() {
+  }
+
+  first() {
+  }
+
+  last() {
+  }
+
+  isEmpty() {
+  }
+
+  isFull() {
+  }
+
+  stackPeep() {
+  }
+
+  // implement queue
+  queue() {
+  }
+}
+
+module.exports = Stack
