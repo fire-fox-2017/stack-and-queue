374be2067cb9047e11221d42fd9f103cbe5d2ddd
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-03-29 18:51:23 +0700

    Stack and Queue selesai

--- queue.js
+++ queue.js
@@ -1,7 +1,80 @@
 'use strict'
 
 class Queue {
-  constructor () {
+    constructor() {
+        this.max_size = 5;
+        this.objQueue = [];
+    }
 
-  }
+    showAll() {
+        console.log('Data di queue : ' + this.objQueue);
+    }
+
+    size() {
+        console.log('Ukuran queue : ' + this.objQueue.length);
+    }
+
+    queuePush(x) {
+      if(this.objQueue.length === this.max_size){
+        console.log('Queue Penuh');
+      } else {
+        this.objQueue.push(x);
+        console.log('Berhasil menambah ' + x);
+      }
+    }
+
+    queuePop() {
+        this.objQueue.shift();
+    }
+
+    first() {
+        if (this.objQueue[0] === null) {
+            console.log('Queue Kosong');
+        } else {
+            console.log(this.objQueue[0]);
+        }
+    }
+
+    last() {
+        console.log(this.objQueue[this.objQueue.length-1]);
+    }
+
+    isEmpty() {
+        if (this.objQueue.length !== this.max_size) {
+            console.log('Queue Kosong');
+        } else {
+            console.log('Queue Penuh');
+        }
+    }
+
+    isFull() {
+        if (this.objQueue.length === this.max_size) {
+            console.log('Queue Penuh');
+        } else {
+            console.log('Queue Kosong');
+        }
+    }
+
+    queuePeep() {
+        console.log(this.objQueue[this.objQueue.length-1]);
+    }
 }
+
+let queue1 = new Queue;
+queue1.queuePush('Mangga');
+queue1.showAll();
+queue1.queuePush('Melon');
+queue1.showAll();
+queue1.queuePush('Melon');
+queue1.showAll();
+queue1.queuePush('Melon');
+queue1.showAll();
+queue1.queuePush('Melon');
+queue1.showAll();
+queue1.queuePush('Melon');
+queue1.showAll();
+queue1.queuePush('Melon');
+queue1.isFull();
+queue1.isEmpty();
+queue1.queuePop();
+queue1.showAll();
--- stack.js
+++ stack.js
@@ -2,36 +2,81 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
-  }
+    constructor() {
+        this.max_size = 5;
+        this.objStack = [];
+    }
 
-  size() {
-  }
+    showAll() {
+        console.log('Data di stack : ' + this.objStack);
+    }
 
-  stackPush(x) {
-  }
+    size() {
+        console.log('Ukuran stack : ' + this.objStack.length);
+    }
 
-  stackPop() {
-  }
+    stackPush(x) {
+      if(this.objStack.length === this.max_size){
+        console.log('Stack Penuh');
+      } else {
+        this.objStack.push(x);
+        console.log('Berhasil menambah ' + x);
+      }
+    }
 
-  first() {
-  }
+    stackPop() {
+        this.objStack.pop();
+    }
 
-  last() {
-  }
+    first() {
+        console.log(this.objStack[this.objStack.length-1]);
+    }
 
-  isEmpty() {
-  }
+    last() {
+        if (this.objStack[0] === null) {
+            console.log('Stack Kosong');
+        } else {
+            console.log(this.objStack[0]);
+        }
+    }
 
-  isFull() {
-  }
+    isEmpty() {
+        if (this.objStack.length !== this.max_size) {
+            console.log('Stack Kosong');
+        } else {
+            console.log('Stack Penuh');
+        }
+    }
 
-  stackPeep() {
-  }
+    isFull() {
+        if (this.objStack.length === this.max_size) {
+            console.log('Stack Penuh');
+        } else {
+            console.log('Stack Kosong');
+        }
+    }
 
-  // implement queue
-  queue() {
-  }
+    stackPeep() {
+        console.log(this.objStack[this.objStack.length-1]);
+    }
+
 }
 
-module.exports = Stack
+let stack1 = new Stack;
+stack1.stackPush('Mangga');
+stack1.showAll();
+stack1.stackPush('Melon');
+stack1.showAll();
+stack1.stackPush('Melon');
+stack1.showAll();
+stack1.stackPush('Melon');
+stack1.showAll();
+stack1.stackPush('Melon');
+stack1.showAll();
+stack1.stackPush('Melon');
+stack1.showAll();
+stack1.stackPush('Melon');
+stack1.isFull();
+stack1.isEmpty();
+
+// module.exports = Stack
