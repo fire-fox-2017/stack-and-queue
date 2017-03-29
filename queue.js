077b86b76943f291d521ae43839948e7dbe7d704
ridho0 <ridho230994@gmail.com>
2017-03-29 14:06:54 +0700

    selesai

--- queue.js
+++ queue.js
@@ -2,6 +2,21 @@
 
 class Queue {
   constructor () {
+    this.wadah = []
+  }
 
+  insert (insert) {
+    this.wadah.push(insert)
+  }
+
+  remove () {
+    this.wadah.shift();
   }
 }
+
+let fifo = new Queue();
+fifo.insert("pertama");
+fifo.remove();
+fifo.insert(2);
+fifo.insert(true);
+console.log(fifo.wadah);
