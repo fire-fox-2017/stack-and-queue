9da2414156544d05f4a98c39c99c9a6be485c635
ridho0 <ridho230994@gmail.com>
2017-03-29 17:55:53 +0700

    selesai

--- queue.js
+++ queue.js
@@ -2,15 +2,45 @@
 
 class Queue {
   constructor () {
-    this.wadah = []
+    this.antrian = []
+    this.kapasitasAntrian = 5
+  }
+
+  size() {
+    return this.kapasitasAntrian;
   }
 
   insert (insert) {
-    this.wadah.push(insert)
+    if(this.antrian.length < this.kapasitasAntrian){
+      this.antrian.push(insert)
+    } else {
+      console.log("maaf antrian penuh")
+    }
+
   }
 
   remove () {
-    this.wadah.shift();
+    this.antrian.shift();
+  }
+
+  first() {
+    return `first : ${this.antrian[0]}`
+  }
+
+  last() {
+    return `last : ${this.antrian[this.antrian.length-1]}`
+  }
+
+  isEmpty() {
+    return this.antrian.length == 0
+  }
+
+  isFull() {
+    return this.antrian.length == this.kapasitasAntrian;
+  }
+
+  queuePeep() {
+    return this.antrian.length;
   }
 }
 
@@ -19,4 +49,10 @@ fifo.insert("pertama");
 fifo.remove();
 fifo.insert(2);
 fifo.insert(true);
-console.log(fifo.wadah);
+console.log(`apakah antriannya kosong? ${fifo.isEmpty()}`);
+fifo.insert("hai");
+fifo.insert("tambah dong");
+console.log(`apakah antriannya penuh? ${fifo.isFull()}`);
+fifo.insert("masih bisa ga?");
+fifo.insert("satu lagi ya");
+console.log(fifo.antrian);
