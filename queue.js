4b298f46f6f59291c2de020189c4add442fcd39a
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-03-29 20:35:53 +0700

    Komit1 20.35 29-3-17

--- queue.js
+++ queue.js
@@ -2,6 +2,72 @@
 
 class Queue {
   constructor () {
+    this.queue = [];
+  }
 
+  size() {
+    return this.queue.length;
+  }
+
+  queuePush(x) {
+    this.queue.push(x);
+  }
+
+  queuePop() {
+    return this.queue.pop();
+  }
+
+  queuePeek() {
+    return this.queue[0];
+  }
+
+  first() {
+    return this.queue[0];
+  }
+
+  last() {
+    return this.queue[size()-1];
+  }
+
+  isEmpty() {
+    if (this.size() == 0){
+      return "true";
+    } else {
+      return "false";
+    }
+  }
+
+  isFull() {
+    if (this.size() == this.maxsize){
+      return "true";
+    } else {
+      return "false";
+    }
+  }
+
+  queuePeep() {
+    return this.last();
+  }
+
+  queuePush(x){
+    return this.queue.push(x);
+  }
+
+  queuePop(){
+    return this.queue.shift();
   }
 }
+
+let myQueue = new Queue()
+console.log(`The queue is empty? ${myQueue.isEmpty()}`)
+myQueue.queuePush("JavaScript");
+myQueue.queuePush("is just so");
+myQueue.queuePush("cool");
+myQueue.queuePush(15);
+myQueue.queuePeek();
+console.log(myQueue.queuePop());
+console.log(myQueue.queuePop());
+console.log(myQueue.queuePop());
+console.log(myQueue.queuePop());
+console.log(`The queue is empty? ${myQueue.isEmpty()}`)
+console.log(myQueue.queuePop())
--- stack.js
+++ stack.js
@@ -3,35 +3,72 @@
 // Your code implementation
 class Stack {
   constructor() {
+    this.stack = [];
+    this.maxsize = 5;
   }
 
   size() {
+    return this.stack.length;
   }
 
   stackPush(x) {
+    this.stack.push(x);
   }
 
   stackPop() {
+    return this.stack.pop();
+  }
+
+  stackPeek() {
+    return this.stack[0];
   }
 
   first() {
+    return this.stack[0];
   }
 
   last() {
+    return this.stack[size()-1];
   }
 
   isEmpty() {
+    if (this.size() == 0){
+      return "true";
+    } else {
+      return "false";
+    }
   }
 
   isFull() {
+    if (this.size() == this.maxsize){
+      return "true";
+    } else {
+      return "false";
+    }
   }
 
   stackPeep() {
+    return this.last();
   }
 
   // implement queue
   queue() {
+
   }
 }
+
+let myStack = new Stack()
+console.log(`The stack is empty? ${myStack.isEmpty()}`)
+myStack.stackPush("JavaScript");
+myStack.stackPush("is just so");
+myStack.stackPush("cool");
+myStack.stackPush(15);
+myStack.stackPeek();
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(`The stack is empty? ${myStack.isEmpty()}`)
+console.log(myStack.stackPop())
 
 module.exports = Stack
