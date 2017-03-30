e15004dec9061d8b30e31bb1e79dad529d60e5e7
arahito <uarahito@yahoo.com>
2017-03-30 07:48:13 +0700

    Stack and Queue

--- queue.js
+++ queue.js
@@ -2,6 +2,65 @@
 
 class Queue {
   constructor () {
+    this.max_size = 4;
+    this.array = [];
+  }
 
+  size() {
+    return this.array.length;
+  }
+
+  queueInsert(x) {
+    if (this.array.length === this.max_size) {
+      console.log("Full");
+    } else {
+      console.log(x);
+      this.array.unshift(x);
+    }
+  }
+
+  queueRemove() {
+    if (this.array.length === 0) {
+      console.log("Empty");
+    } else {
+      this.array.shift();
+    }
+  }
+
+  first() {
+    return this.array[0];
+  }
+
+  last() {
+    return this.array[this.array.length-1];
+  }
+
+  isEmpty() {
+    if (this.array.length === 0) {
+      return 'Empty';
+    } else {
+      return 'Not Empty';
+    }
+  }
+
+  isFull() {
+    if (this.array.length === this.max_size) {
+      return 'Full';
+    } else {
+      return 'Not Full';
+    }
   }
 }
+
+let queue = new Queue()
+console.log(queue.size());
+queue.queueInsert('jkjkj');
+queue.queueInsert('bgbgbg');
+queue.queueInsert('jujuj');
+console.log(queue.array);
+queue.queueRemove();
+console.log(queue.array);
+console.log(queue.first());
+console.log(queue.last());
+console.log(queue.isEmpty());
+console.log(queue.isFull());
--- stack.js
+++ stack.js
@@ -3,35 +3,75 @@
 // Your code implementation
 class Stack {
   constructor() {
+    this.max_size = 4;
+    this.array = [];
   }
 
   size() {
+    return this.array.length;
   }
 
   stackPush(x) {
+    if (this.array.length === this.max_size) {
+      console.log("Full");
+    } else {
+      console.log(x);
+      this.array.push(x);
+    }
   }
 
   stackPop() {
+    if (this.array.length === 0) {
+      console.log("Empty");
+    } else {
+      this.array.pop();
+    }
   }
 
   first() {
+    return this.array[0];
   }
 
   last() {
+    return this.array[this.array.length-1];
   }
 
   isEmpty() {
+    if (this.array.length === 0) {
+      return 'Empty';
+    } else {
+      return 'Not Empty';
+    }
   }
 
   isFull() {
+    if (this.array.length === this.max_size) {
+      return 'Full';
+    } else {
+      return 'Not Full';
+    }
   }
 
-  stackPeep() {
-  }
-
-  // implement queue
-  queue() {
-  }
+  // stackPeep() {
+  // }
+  //
+  // // implement queue
+  // queue() {
+  // }
 }
+
+let stack = new Stack();
+
+console.log(stack.size());
+stack.stackPush('erer');
+stack.stackPush('eywueyw');
+stack.stackPush('udhjh');
+console.log(stack.array);
+stack.stackPop();
+console.log(stack.array);
+console.log(stack.first());
+console.log(stack.last());
+console.log(stack.isEmpty());
+console.log(stack.isFull());
 
 module.exports = Stack
