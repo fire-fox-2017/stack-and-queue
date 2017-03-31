8df2a9da1e1650fafd6592d9e4b78fa12d125519
edim dendy &   "name": null <pair+edimdendidiwangga.zanisis@hacktiv8.com>
2017-03-31 13:32:02 +0700

    stack queue - edim

--- queue.js
+++ queue.js
@@ -1,7 +1,62 @@
-'use strict'
-
+// FIFO
 class Queue {
   constructor () {
+    this.arr = []
+    this.MAX_SIZE = 5
+  }
 
+  queueInsert(x) {
+    if (this.isFull()) {
+      console.log(`queue is full, you can't add more queue`);
+    } else {
+      this.arr.unshift(x)
+    }
+  }
+
+  queueRemove() {
+    return this.arr.shift()
+  }
+
+  queuePeek() {
+    return this.arr
+  }
+
+  isEmpty () {
+    if (this.arr.length == 0) {
+      return `yes it's empty`
+    } else {
+      return `no, it's: ${this.queuePeek()}`
+    }
+  }
+
+  isFull() {
+    if (this.arr.length >= this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
   }
 }
+
+
+let myQueue = new Queue()
+console.log(`The Queue is empty? ${myQueue.isEmpty()}`);
+
+myQueue.queueInsert(15)
+myQueue.queueInsert('hacktiv8')
+myQueue.queueInsert('full stack')
+myQueue.queueInsert('web developer')
+myQueue.queueInsert('javascript')
+myQueue.queueInsert('HTML')
+console.log(myQueue.queuePeek())
+
+console.log(myQueue.isFull());
+console.log(`The stack is empty? ${myQueue.isEmpty()}`);
+
+console.log(`removing queue: ${myQueue.queueRemove()}`);
+console.log(`removing queue: ${myQueue.queueRemove()}`);
+console.log(`removing queue: ${myQueue.queueRemove()}`);
+console.log(`removing queue: ${myQueue.queueRemove()}`);
+// console.log(myQueue);
+myQueue.queueInsert('HTML')
+console.log(myQueue.queuePeek());
--- stack.js
+++ stack.js
@@ -1,37 +1,64 @@
 'use strict'
-
-// Your code implementation
+// LIFO
 class Stack {
-  constructor() {
+  constructor () {
+    this.arr = []
+    this.MAX_SIZE = 5
   }
 
-  size() {
+  stackPush (x) {
+    if (this.isFull()) {
+      console.log(`Stack is full, you can't add more stack`);
+    } else {
+      this.arr.push(x)
+    }
+    console.log(this.arr);
   }
 
-  stackPush(x) {
+  stackPop () {
+    return this.arr.pop()
   }
 
-  stackPop() {
+  stackPeek () {
+    return this.arr
   }
 
-  first() {
-  }
-
-  last() {
-  }
-
-  isEmpty() {
+  isEmpty () {
+    if (this.arr.length == 0) {
+      return `yes it's empty`
+    } else {
+      return `no, it's: ${this.stackPeek()}`
+    }
   }
 
   isFull() {
-  }
-
-  stackPeep() {
-  }
-
-  // implement queue
-  queue() {
+    if (this.arr.length >= this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
   }
 }
 
-module.exports = Stack
+let myStack = new Stack()
+
+console.log(`The stack is empty? ${myStack.isEmpty()}`);
+
+myStack.stackPush(15)
+myStack.stackPush('hacktiv8')
+myStack.stackPush('full stack')
+myStack.stackPush('web developer')
+myStack.stackPush('javascript')
+myStack.stackPush('HTML')
+console.log(myStack.stackPeek())
+
+console.log(myStack.isFull());
+console.log(`The stack is empty? ${myStack.isEmpty()}`);
+
+console.log(`removing stack: ${myStack.stackPop()}`);
+console.log(`removing stack: ${myStack.stackPop()}`);
+console.log(`removing stack: ${myStack.stackPop()}`);
+console.log(`removing stack: ${myStack.stackPop()}`);
+// console.log(myStack);
+myStack.stackPush('HTML')
+console.log(myStack.stackPeek());
