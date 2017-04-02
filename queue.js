bf32c00d9750f527f312353c6493e43e42b8fcce
ambo-dalle <ambodalle.st@gmail.com>
2017-04-02 18:00:38 +0700

    stack dan queue selesai

--- queue.js
+++ queue.js
@@ -1,7 +1,72 @@
-'use strict'
-
-class Queue {
-  constructor () {
-
+class Queue{
+  constructor() {
+    this.data = [];
+    this.maxSize = 6;
   }
+
+  size() {
+    return this.maxSize.length;
+  }
+
+  QueuePush(x) {
+    if(this.isFull()) {
+      return console.log(`Queue Penuh, Data Maximal ${this.maxSize}`);
+    } else {
+      this.data.push(x);
+      return console.log(`Berhasil menambah data ${x}`);
+    }
+  }
+
+  QueueShift() {
+    if (this.isEmpty()) {
+      return console.log(`Data di Queue Kosong`);
+    } else {
+      this.data.shift();
+      return console.log(`Berhasil  menghapus data ${this.data.shift()}`);
+    }
+  }
+
+  Queue_first() {
+    if (this.data[0] === null) {
+      return console.log(`Data Queue Kosong`);
+    } else  {
+      return console.log(this.data[0]);
+    }
+  }
+
+  Queue_last() {
+      return console.log(this.data[this.data.length - 1]);
+  }
+
+  isEmpty() {
+    if (this.data.length === 0) {
+      return console.log(`Queue Kosong`);
+    } 
+  }
+
+  isFull() {
+    return this.size() == this.maxSize;
+  }
+
+  QueuePeep() {
+    return this.Queue_last();
+  }
+
 }
+
+let DataNew = new Queue();
+console.log(`The Queue Kosong? ${DataNew.isEmpty()}`);
+DataNew.QueuePush("JavaScript");
+DataNew.QueuePush("is just so");
+DataNew.QueuePush("cool");
+DataNew.QueuePush(15);
+console.log(DataNew.data);
+DataNew.QueuePeep();
+console.log(DataNew.QueueShift());
+console.log(DataNew.QueueShift());
+console.log(DataNew.QueueShift());
+console.log(DataNew.QueueShift());
+console.log(`The Queue is empty? ${DataNew.isEmpty()}`);
+console.log(DataNew.QueueShift());
+
+module.exports = Queue
--- stack.js
+++ stack.js
@@ -1,37 +1,97 @@
-'use strict'
+ 'use strict'
 
 // Your code implementation
 class Stack {
   constructor() {
+    this.data = [];
+    this.maxSize = 6;
   }
 
   size() {
+    return this.maxSize.length;
   }
 
   stackPush(x) {
+    if(this.data.length === this.maxSize ) {
+      return console.log(`Stack Penuh, Data Maximal ${this.maxSize}`);
+    } else {
+      this.data.push(x);
+      return console.log(`Berhasil menambah data ${x}`);
+    }
   }
 
   stackPop() {
+    if (this.data.length === 0) {
+      return console.log(`Data di Stak Kosong`);
+    } else {
+      return console.log(`Berhasil  menghapus data ${this.data.pop()}`);
+    }
   }
 
   first() {
+    console.log(this.data[this.data.length - 1]);
   }
 
   last() {
+    if (this.data[0] === null) {
+      return console.log(`Data Stack Kosong`);
+    } else  {
+      return console.log(this.data[0]);
+    }
   }
 
   isEmpty() {
+    if (this.data.length === 0) {
+      return console.log(`Data Kosong`);
+    } else {
+      return console.log(`Stack tidak kosong, Jumlah data : ${this.data.length}`);
+    }
   }
 
   isFull() {
+    if (this.data.length === this.maxSize) {
+      return console.log(`Stak Penuh!!`);
+    } else if(this.data.length === 0){
+       return console.log(`Data di Stack Kosong`);
+    } else {
+        return console.log(`Stack Belum Penuh, Data di dalam stack : ${this.data.length}`);
+    }
   }
 
   stackPeep() {
+    return this.data[this.data.length - 1];
   }
 
-  // implement queue
-  queue() {
-  }
+  // // implement queue
+  // queue() {
+  // }
 }
+
+
+let mobil = new Stack;
+mobil.isEmpty();
+mobil.stackPush('Fortuner');
+console.log(mobil.data);
+mobil.stackPush('Lamborgini');
+console.log(mobil.data);
+mobil.stackPush('Ferary');
+console.log(mobil.data);
+
+mobil.isEmpty();
+mobil.stackPush('Lexus');
+console.log(mobil.data);
+mobil.stackPush('Jazz');
+console.log(mobil.data);
+mobil.stackPush('Avanza');
+console.log(mobil.data);
+mobil.stackPush('xenia');
+console.log(mobil.data);
+mobil.stackPush('Luxio');
+console.log(mobil.data);
+
+mobil.isFull();
+mobil.stackPop();
+console.log(mobil.data);
+
 
 module.exports = Stack
