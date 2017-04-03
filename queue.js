065b8c8e159a8047402ae84fd766b1cb3ab4fcdf
dyankastutara <dyankastutara19@gmail.com>
2017-04-03 08:55:14 +0700

    dyankastutara done

--- queue.js
+++ queue.js
@@ -2,6 +2,64 @@
 
 class Queue {
   constructor () {
-
+    this.MAX_SIZE = 5;
+    this.EMPTY = 0;
+    this.arrQueue = [];
+  }
+  size(){
+    return this.arrQueue.length;
+  }
+  queuePush(data){
+    if(this.isFull()){
+      console.log("Queue is FUll!!!")
+    }else{
+    return this.arrQueue.push(data);
+    }
+  }
+  queueShift(){
+    if(this.isEmpty()){
+      console.log("Queue is Empty!!!");
+    }else{
+    return this.arrQueue.shift();
+    }
+  }
+  isFull(){
+    return this.size() == this.MAX_SIZE;
+  }
+  isEmpty(){
+    return this.size() == this.EMPTY;
+  }
+  first(){
+    return this.arrQueue[0];
+  }
+  last(){
+    return this.arrQueue[this.size() - 1];
   }
 }
+
+let myQueue = new Queue();
+console.log(`The queue is empty? ${myQueue.isEmpty()}`);
+myQueue.queuePush("JavaScript");
+console.log(myQueue.arrQueue);
+myQueue.queuePush("is just so");
+console.log(myQueue.arrQueue);
+myQueue.queuePush("cool");
+console.log(myQueue.arrQueue);
+myQueue.queuePush(15);
+console.log(myQueue.arrQueue);
+myQueue.queuePush(1);
+console.log(myQueue.arrQueue);
+myQueue.queuePush(2);
+console.log(myQueue.arrQueue);
+
+console.log(myQueue.queueShift());
+console.log(myQueue.arrQueue);
+console.log(myQueue.queueShift());
+console.log(myQueue.arrQueue);
+console.log(myQueue.queueShift());
+console.log(myQueue.arrQueue);
+console.log(myQueue.queueShift());
+console.log(myQueue.arrQueue);
+console.log(myQueue.queueShift());
+console.log(myQueue.arrQueue);
+console.log(`The queue is empty? ${myQueue.isEmpty()}`);
--- stack.js
+++ stack.js
@@ -3,35 +3,66 @@
 // Your code implementation
 class Stack {
   constructor() {
+  this.MAX_SIZE = 5;
+  this.EMPTY = 0;
+  this.arrStack = [];
   }
-
   size() {
+    return this.arrStack.length;
   }
 
-  stackPush(x) {
+  stackPush(data) {
+    if(this.isFull()){
+      return "Stack Full !!!";
+    }else{
+     return this.arrStack.push(data);
+    }
   }
 
   stackPop() {
+    if(this.isEmpty()){
+      return "Stack Empty !!!";
+    }else{
+    return this.arrStack.pop();
+    }
   }
 
   first() {
+    return this.arrStack[0];
   }
 
   last() {
+    return this.arrStack[this.size()-1];
   }
 
   isEmpty() {
+    return this.size() == this.EMPTY;
   }
 
   isFull() {
-  }
-
-  stackPeep() {
+    return this.size() == this.MAX_SIZE;
   }
 
   // implement queue
   queue() {
   }
-}
 
+}
+let myStack = new Stack();
+console.log(`The stack is empty? ${myStack.isEmpty()}`);
+console.log(myStack.stackPush("JavaScript")+" "+myStack.arrStack);
+console.log(myStack.stackPush("Hacktiv8")+" "+myStack.arrStack);
+console.log(myStack.stackPush("Fire Fox")+" "+myStack.arrStack);
+console.log(myStack.stackPush(15)+" "+myStack.arrStack);
+console.log(myStack.stackPush(1)+" "+myStack.arrStack);
+console.log(myStack.stackPush(2)+" \nLast data is : "+myStack.arrStack);
+
+console.log("Data pop : "+myStack.stackPop()+" \nLast data is : "+myStack.arrStack);
+console.log("Data pop : "+myStack.stackPop()+" \nLast data is : "+myStack.arrStack);
+console.log("Data pop : "+myStack.stackPop()+" \nLast data is : "+myStack.arrStack);
+console.log("Data pop : "+myStack.stackPop()+" \nLast data is : "+myStack.arrStack);
+console.log("Data pop : "+myStack.stackPop()+" \nLast data is : "+myStack.arrStack);
+console.log("Data pop : "+myStack.stackPop()+" \nLast data is : "+myStack.arrStack);
+console.log(myStack.arrStack);
+console.log(`The stack is empty? ${myStack.isEmpty()}`);
 module.exports = Stack
